ea2c5ef000ce76e048e4d38df13605a37dcac85c
Cristóbal Iturralde Montes <citurralde@gmail.com>
2022-09-06 19:31:43 -0500

    Carga Inicial del Proyecto

---
+++ Models/TestService.cs
@@ -0,0 +1,26 @@
+﻿namespace NetProgPractices.Models
+{
+    public class TestService
+    {
+
+        public Student getStudent ()
+        {
+
+            Student newStudent = new();
+
+            newStudent = new Student { IdStudent = 1, StudentName = "Primer Alumno" };
+
+            return newStudent;
+
+        }
+
+    }
+
+    public class Student
+    {
+        public int IdStudent { get; set; }
+
+        public string?  StudentName { get; set; }
+    }
+
+}
